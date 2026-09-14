8bb1bc873c8bd38fc756f8ef5aff2d4e52452e01
jjjonghyeon <whdgus6653@naver.com>
2026-09-14 16:15:57 +0900

    feat(content)/콘텐츠 투표,알림 엔í티티 초안

---
+++ backend/src/main/java/com/prgms/backend/domain/content/ENUM/ContentPollStatus.java
@@ -0,0 +1,6 @@
+package com.prgms.backend.domain.content.ENUM;
+
+public enum ContentPollStatus {
+    OPEN, // 투표판이 열려있음
+    CLOSED
+}
---
+++ backend/src/main/java/com/prgms/backend/domain/notification/ENUM/NotificationType.java
@@ -0,0 +1,8 @@
+package com.prgms.backend.domain.notification.ENUM;
+
+// 각 상태별 알림
+public enum NotificationType {
+    SCHEDULE_CLOSED, // 일정 마감
+    CONTENT_CLOSED, // 콘텐츠 마감
+    SETTLEMENT_FINALIZED // 정산 마감
+}
