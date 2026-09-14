a1f454f63b39fa1c529ba98317f9498248e09d7f
alswn <alswn12004@gmail.com>
2026-09-14 13:59:31 +0900

    feat/ecpense

---
+++ backend/src/main/java/com/prgms/backend/domain/expense/entity/Expense.java
@@ -0,0 +1,12 @@
+package com.prgms.backend.domain.expense.entity;
+
+public class Expense {
+
+    String content;
+    int totalCost;
+    user user;
+    boolean status;
+    date created;
+    date ended;
+
+}
