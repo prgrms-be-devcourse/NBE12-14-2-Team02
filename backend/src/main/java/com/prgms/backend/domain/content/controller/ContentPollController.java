5e557b1a5e27665773ac0bdafce267d984587f89
jjjonghyeon <whdgus6653@naver.com>
2026-09-15 12:32:56 +0900

    feat/ 콘텐츠 í투표 생성 API 추ë가

---
+++ backend/src/main/java/com/prgms/backend/domain/content/controller/ContentPollController.java
@@ -0,0 +1,28 @@
+package com.prgms.backend.domain.content.controller;
+
+import com.prgms.backend.domain.content.dto.ContentPollCreateRequest;
+import com.prgms.backend.domain.content.dto.ContentPollResponse;
+import com.prgms.backend.domain.content.service.ContentPollService;
+import com.prgms.backend.global.ApiResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/meeting/{meetingId}/content-poll")
+public class ContentPollController {
+    private final ContentPollService contentPollService;
+
+    @PostMapping
+    public ResponseEntity<ApiResponse<ContentPollResponse>> create(
+            @PathVariable Long meetingId,
+            @Valid @RequestBody ContentPollCreateRequest request
+            ){
+        ContentPollResponse response = contentPollService.create(meetingId, request);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(ApiResponse.success(201,response));
+    }
+}
---
+++ backend/src/main/java/com/prgms/backend/domain/content/service/ContentPollService.java
@@ -0,0 +1,36 @@
+package com.prgms.backend.domain.content.service;
+
+import com.prgms.backend.domain.content.dto.ContentPollCreateRequest;
+import com.prgms.backend.domain.content.dto.ContentPollResponse;
+import com.prgms.backend.domain.content.entity.ContentPoll;
+import com.prgms.backend.domain.content.repository.ContentPollRepository;
+import com.prgms.backend.domain.meeting.repository.MeetingRepository;
+import com.prgms.backend.global.exception.custom.ContentPollAlreadyExistsException;
+import com.prgms.backend.global.exception.custom.MeetingNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class ContentPollService {
+    private final ContentPollRepository contentPollRepository;
+    private final MeetingRepository meetingRepository;
+
+    @Transactional
+    public ContentPollResponse create(Long meetingId, ContentPollCreateRequest request){
+        if(!meetingRepository.existsById(meetingId)){
+            throw new MeetingNotFoundException(meetingId);
+        }
+
+        if (contentPollRepository.existsByMeetingId(meetingId)){
+            throw new ContentPollAlreadyExistsException(meetingId);
+        }
+
+        ContentPoll saved = contentPollRepository.save(
+                new ContentPoll(meetingId,request.deadline())
+        );
+
+        return ContentPollResponse.from(saved);
+    }
+}
