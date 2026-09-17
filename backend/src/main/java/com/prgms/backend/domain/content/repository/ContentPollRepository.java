fc11d6fa160d14b431c2576c714c87d24dbbc9a3
jjjonghyeon <whdgus6653@naver.com>
2026-09-17 12:38:54 +0900

    feat/콘텐츠 투표 마감기한 시 투표판 닫힘 구현

--- backend/src/main/java/com/prgms/backend/domain/content/repository/ContentPollRepository.java
+++ backend/src/main/java/com/prgms/backend/domain/content/repository/ContentPollRepository.java
@@ -1,8 +1,11 @@
 package com.prgms.backend.domain.content.repository;
 
+import com.prgms.backend.domain.content.ENUM.ContentPollStatus;
 import com.prgms.backend.domain.content.entity.ContentPoll;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface ContentPollRepository extends JpaRepository<ContentPoll,Long> {
@@ -10,4 +13,9 @@ public interface ContentPollRepository extends JpaRepository<ContentPoll,Long> {
 
     // 이 모임에 투표판이 존제는지에 대한 함수
     boolean existsByMeetingId(Long meetingId);
+
+    List<ContentPoll> findByStatusAndDeadlineLessThanEqual(
+            ContentPollStatus status,
+            LocalDateTime now
+    );
 }
